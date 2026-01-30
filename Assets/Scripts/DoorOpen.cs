dca18dd6b504d132e21bfc91d8c2cc6eac770811
blueberry <ansisiddhu@gmail.com>
2026-01-31 03:33:02 +0530

    door bug fixed (ig)

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -3,57 +3,52 @@ using UnityEngine;
 public class DoorOpen : MonoBehaviour
 {
     public ButtonScript button;
-
     public Transform doorPart1;
     public Transform doorPart2;
-
-    public Vector2 openPos1;
-    public Vector2 openPos2;
-
+    
+    // Use Vector3 for full control
+    public Vector3 openPos1;
+    public Vector3 openPos2;
+    
     public float openSpeed = 5f;
     public float closeSpeed = 3f;
-
+    
     private Vector3 startPos1;
     private Vector3 startPos2;
-
+    
     void Start()
     {
-        startPos1 = doorPart1.position;
-        startPos2 = doorPart2.position;
+        // Store LOCAL positions instead of world positions
+        startPos1 = doorPart1.localPosition;
+        startPos2 = doorPart2.localPosition;
     }
-
+    
     void Update()
     {
-        Vector3 targetPos1;
-        Vector3 targetPos2;
-
         if (button != null && button.underCollision)
         {
-            targetPos1 = new Vector3(openPos1.x, openPos1.y, startPos1.z);
-            targetPos2 = new Vector3(openPos2.x, openPos2.y, startPos2.z);
-
-            doorPart1.position = Vector3.MoveTowards(
-                doorPart1.position,
-                targetPos1,
+            // Open the door - use localPosition
+            doorPart1.localPosition = Vector3.MoveTowards(
+                doorPart1.localPosition,
+                openPos1,
                 openSpeed * Time.deltaTime
             );
-
-            doorPart2.position = Vector3.MoveTowards(
-                doorPart2.position,
-                targetPos2,
+            doorPart2.localPosition = Vector3.MoveTowards(
+                doorPart2.localPosition,
+                openPos2,
                 openSpeed * Time.deltaTime
             );
         }
         else
         {
-            doorPart1.position = Vector3.MoveTowards(
-                doorPart1.position,
+            // Close the door - use localPosition
+            doorPart1.localPosition = Vector3.MoveTowards(
+                doorPart1.localPosition,
                 startPos1,
                 closeSpeed * Time.deltaTime
             );
-
-            doorPart2.position = Vector3.MoveTowards(
-                doorPart2.position,
+            doorPart2.localPosition = Vector3.MoveTowards(
+                doorPart2.localPosition,
                 startPos2,
                 closeSpeed * Time.deltaTime
             );
